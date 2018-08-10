b53418e32ab786d8fcf40b47edd9ad4a879c7fc6
Bin Zhu <bzhu@icontrol.com>
2018-08-10 18:21:20 -0500

    Initial import

---
+++ src/main/java/book/BookController.java
@@ -0,0 +1,14 @@
+package book;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class BookController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Boo Service!";
+    }
+
+}
